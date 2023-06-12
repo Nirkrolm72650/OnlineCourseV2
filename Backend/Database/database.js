e98d064d27e025933a68890b6315599461ff902a
Nirkrolm72650 <dev.brandonguyon@gmail.com>
2023-06-12 04:15:29 +0200

    Add console.log

--- Backend/Database/database.js
+++ Backend/Database/database.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose
+      .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(console.log("Connecté à la base de donnée"));
   } catch (error) {
     console.error("Erreur lors de la connexion à la base de donnée. ", error);
   }
